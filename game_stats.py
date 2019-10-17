aeab584b51bb32cddb4df783ba0c207894d6e357
Kevin Rodriguez <32004550+drkfogle@users.noreply.github.com>
2019-10-17 11:25:34 -0400

    Add files via upload

---
+++ game_stats.py
@@ -0,0 +1,16 @@
+
+
+class GameStats:
+    def __init__(self, ai_settings):
+        self.high_score = 0
+        self.ai_settings = ai_settings
+        self.reset_stats()
+
+        self.game_active = False
+
+
+
+    def reset_stats(self):
+        self.ships_left = self.ai_settings.ships_limit
+        self.score = 0
+        self.level = 1
